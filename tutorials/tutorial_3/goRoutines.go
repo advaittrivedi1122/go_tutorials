089d13ad9596b5e790b04c0cf631e0e81913f1b6
Advait Trivedi <trivediadvait0@gmail.com>
2023-10-15 19:07:48 +0530

    understand how waitGroup works

--- tutorials/tutorial_3/goRoutines.go
+++ tutorials/tutorial_3/goRoutines.go
@@ -1,22 +1,27 @@
 package main
 import "fmt"
-import "time"
 import "sync"
 var wg sync.WaitGroup
 
 func async(num int){
 	defer wg.Done()
 	fmt.Println("async code first", num)
-	time.Sleep(time.Second)
 	fmt.Println("async code second", num)
 }
 
 func main(){
+
 	wg.Add(1)
+	go async(11)
+	wg.Wait() // will wait for 11
+	
 	wg.Add(1)
-	go async(1)
-	go async(2)
+	go async(22)
+	
 	fmt.Println("sync code")
-	wg.Wait()
+	
+	wg.Add(1)
+	go async(33)
+	wg.Wait() // will wait for all pending added wg till here (22,33)
 }
 
